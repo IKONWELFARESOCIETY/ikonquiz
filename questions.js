23bfcc62ebdedc5d7439dacad66d22adf55e0684
IKONWELFARESOCIETY <samadkhanabdul465@gmail.com>
2026-07-13 22:04:08 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,113 @@
+const questions = [
+
+{
+    question:"MS Excel me Formula kis sign se start hota hai?",
+    options:[
+        "#",
+        "=",
+        "%",
+        "$"
+    ],
+    answer:1
+},
+
+{
+    question:"MS Word kis company ka software hai?",
+    options:[
+        "Apple",
+        "Google",
+        "Microsoft",
+        "IBM"
+    ],
+    answer:2
+},
+
+{
+    question:"CPU ka Full Form kya hai?",
+    options:[
+        "Central Processing Unit",
+        "Computer Power Unit",
+        "Central Program Unit",
+        "Control Processing Unit"
+    ],
+    answer:0
+},
+
+{
+    question:"HTML ka Full Form kya hai?",
+    options:[
+        "Hyper Text Markup Language",
+        "High Text Machine Language",
+        "Home Tool Markup Language",
+        "Hyper Transfer Markup Language"
+    ],
+    answer:0
+},
+
+{
+    question:"Internet Browser ka example kaunsa hai?",
+    options:[
+        "MS Paint",
+        "Google Chrome",
+        "Notepad",
+        "Excel"
+    ],
+    answer:1
+},
+
+{
+    question:"Windows kis type ka Software hai?",
+    options:[
+        "System Software",
+        "Application Software",
+        "Programming Software",
+        "Utility Software"
+    ],
+    answer:0
+},
+
+{
+    question:"Excel Workbook me Sheet insert karne ki Shortcut Key kya hai?",
+    options:[
+        "Shift + F11",
+        "Ctrl + S",
+        "Ctrl + N",
+        "Alt + F4"
+    ],
+    answer:0
+},
+
+{
+    question:"PowerPoint kis kaam ke liye use hota hai?",
+    options:[
+        "Presentation",
+        "Programming",
+        "Accounting",
+        "Photo Editing"
+    ],
+    answer:0
+},
+
+{
+    question:"Computer ka Brain kise kaha jata hai?",
+    options:[
+        "RAM",
+        "CPU",
+        "Monitor",
+        "Keyboard"
+    ],
+    answer:1
+},
+
+{
+    question:"Tally Prime ka use kis liye hota hai?",
+    options:[
+        "Accounting",
+        "Gaming",
+        "Programming",
+        "Drawing"
+    ],
+    answer:0
+}
+
+];
