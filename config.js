02617ad9df1b48b7d6ce54f5ab60fcf1ecd434ba
IKONWELFARESOCIETY <samadkhanabdul465@gmail.com>
2026-07-13 22:30:31 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+//==============================
+// IKON ONLINE TEST CONFIG
+//==============================
+
+const testConfig = {
+
+    instituteName: "IKON Institute",
+
+    testTitle: "DAM Class Test",
+
+    testDate: "13 July 2026",
+
+    duration: "30 Minutes"
+
+};
