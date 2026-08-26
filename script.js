b8b596b696e9858d7451c7718b57b1b128e32af1
IKONWELFARESOCIETY <ikonwelfaresociety.gaya@gmail.com>
2026-08-26 20:45:08 +0530

    Clean up script.js by removing blank lines
    Removed unnecessary blank lines in script.js

--- script.js
+++ script.js
@@ -8,11 +8,9 @@
 //====================================================
 const SCRIPT_URL =
 "https://script.google.com/macros/s/AKfycbxvJs4QgvlSBAbcg5zuRyS8TeAzAt-en0h5Kb0V_FUtR6r3HVk-XOxchf0EnKiqEhbr6w/exec";
-
 //====================================================
 // STUDENT DETAILS
 //====================================================
-
 let studentName = "";
 let regNo = "";
 let paperName = "";
